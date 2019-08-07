c63f2f4d4a5ede08b47397dd3b5385c7a63a91ca
Hector <hruiz@opentrends.net>
2019-08-07 23:02:48 +0200

    Set interface and service to connect to api

---
+++ app/src/main/java/com/napptilus/hruiz/willywonka/utils/WillyWonkaService.kt
@@ -0,0 +1,12 @@
+package com.napptilus.hruiz.willywonka.utils
+
+import com.napptilus.hruiz.willywonka.detailscreen.model.entities.EmployeeDetail
+import com.napptilus.hruiz.willywonka.mainscreen.model.entities.Department
+import kotlinx.coroutines.Deferred
+import retrofit2.Response
+
+class WillyWonkaService(private val api: WillyWonkaApi) {
+    fun getEmployeeList(currentPage: Int): Deferred<Response<List<Department>>> = api.getEmployeeList(currentPage)
+
+    fun getEmployeeDetail(employeeId: Int): Deferred<Response<EmployeeDetail>> = api.getEmployeeDetail(employeeId)
+}
